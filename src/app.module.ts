f98ac779b8e4beb3e1dea0b5fe75ae442cd32a89
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-11 22:53:50 -0300

    feat: Authors resource

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,10 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
+import { AuthorsModule } from './authors/authors.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule],
+  imports: [PrismaModule, ArticlesModule, AuthorsModule],
   controllers: [AppController],
   providers: [AppService],
 })
