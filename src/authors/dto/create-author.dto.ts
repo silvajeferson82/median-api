3873a95d95e5b1219e137498c5bb8a3e0a4ca240
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-12 10:33:10 -0300

    feat: define Author entity and DTO

--- src/authors/dto/create-author.dto.ts
+++ src/authors/dto/create-author.dto.ts
@@ -1,1 +1,20 @@
-export class CreateAuthorDto {}
+import { ApiProperty } from '@nestjs/swagger';
+import { IsNotEmpty, IsString, MinLength } from 'class-validator';
+
+export class CreateAuthorDto {
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  name: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @ApiProperty()
+  email: string;
+
+  @IsString()
+  @IsNotEmpty()
+  @MinLength(6)
+  @ApiProperty()
+  password: string;
+}
--- src/authors/entities/author.entity.ts
+++ src/authors/entities/author.entity.ts
@@ -1,1 +1,23 @@
-export class Author {}
+import { ApiProperty } from '@nestjs/swagger';
+import { Author } from '@prisma/client';
+
+export class AuthorEntity implements Author { 
+
+  @ApiProperty()
+  id: string;
+
+  @ApiProperty()
+  createdAt: Date;
+
+  @ApiProperty()
+  updatedAt: Date;
+
+  @ApiProperty()
+  name: string;
+
+  @ApiProperty()
+  email: string;
+
+  password: string;
+
+}
