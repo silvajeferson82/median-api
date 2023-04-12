b294dc7592de0f5eb6eb49ef6228ca8abb6a8e29
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-12 11:34:23 -0300

    feat: config authorController

--- src/authors/authors.controller.ts
+++ src/authors/authors.controller.ts
@@ -1,34 +1,50 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AuthorsService } from './authors.service';
 import { CreateAuthorDto } from './dto/create-author.dto';
 import { UpdateAuthorDto } from './dto/update-author.dto';
+import { ApiCreatedResponse, ApiOkResponse, ApiTags } from '@nestjs/swagger';
+import { AuthorEntity } from './entities/author.entity';
 
 @Controller('authors')
+@ApiTags('authors')
 export class AuthorsController {
   constructor(private readonly authorsService: AuthorsService) {}
 
   @Post()
+  @ApiCreatedResponse({ type: AuthorEntity })
   create(@Body() createAuthorDto: CreateAuthorDto) {
     return this.authorsService.create(createAuthorDto);
   }
 
   @Get()
+  @ApiOkResponse({ type: AuthorEntity, isArray: true })
   findAll() {
     return this.authorsService.findAll();
   }
 
   @Get(':id')
+  @ApiOkResponse({ type: AuthorEntity })
   findOne(@Param('id') id: string) {
-    return this.authorsService.findOne(+id);
+    return this.authorsService.findOne(id);
   }
 
   @Patch(':id')
+  @ApiCreatedResponse({ type: AuthorEntity })
   update(@Param('id') id: string, @Body() updateAuthorDto: UpdateAuthorDto) {
-    return this.authorsService.update(+id, updateAuthorDto);
+    return this.authorsService.update(id, updateAuthorDto);
   }
 
   @Delete(':id')
+  @ApiOkResponse({ type: AuthorEntity })
   remove(@Param('id') id: string) {
-    return this.authorsService.remove(+id);
+    return this.authorsService.remove(id);
   }
 }
