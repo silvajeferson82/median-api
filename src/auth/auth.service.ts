9cda0b45bf9faa920f0409c8cb4a9728b8646b9d
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-12 23:54:13 -0300

    feat: validate hash password

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -6,6 +6,8 @@ import {
 import { JwtService } from '@nestjs/jwt';
 import { PrismaService } from 'src/prisma/prisma.service';
 import { AuthEntity } from './entity/auth.entity';
+import * as bcrypt from 'bcrypt';
+
 
 @Injectable()
 export class AuthService {
@@ -18,7 +20,7 @@ export class AuthService {
       throw new NotFoundException(`No user found for email: ${email}`);
     }
 
-    const isPasswordValid = user.password === password;
+    const isPasswordValid = await bcrypt.compare(password, user.password);
 
     if (!isPasswordValid) {
       throw new UnauthorizedException('Invalid password');
