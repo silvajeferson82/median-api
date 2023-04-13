d47f75d1ea98440d82017f72151711176870239e
Jeferson  Silva <jeferson.silva@3blocks.com.br>
2023-04-12 22:13:56 -0300

    feat: auth resource & config module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,22 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { PrismaModule } from './prisma/prisma.module';
 import { ArticlesModule } from './articles/articles.module';
 import { AuthorsModule } from './authors/authors.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
-  imports: [PrismaModule, ArticlesModule, AuthorsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+    }),
+    PrismaModule,
+    ArticlesModule,
+    AuthorsModule,
+    AuthModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
